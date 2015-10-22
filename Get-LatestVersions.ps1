70580e222e8cf42b6dc90ca1ca9e7d5d6b2b6679
Jasper Nygaard <jasper@sovs.net>
2015-10-22 07:12:10 +0200

    Delete old library file

--- Get-LatestVersions.ps1
+++
@@ -1,8 +0,0 @@
-function Get-LastVersions
-{
-   param([string] $Path,
-         [int]$NumberOfVersions)
-    $result =  gci $path -Directory | Where-Object {$_.Name -match "^(?:(\d+)\.)?(?:(\d+)\.)?(\*|\d+)$"} | Sort-Object $_.Name | select -last $NumberOfVersions
-    return $result
-
-}
